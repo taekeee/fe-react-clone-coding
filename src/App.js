a66338242e03c7e133725bf5e3de753474b00aa4
taek <yeontaek94@naver.com>
2023-05-19 01:47:47 +0900

    Album 페이지 기본 컴퍼넌트 구성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 // 해당 위치에 필요한 컴포넌트를 바탕으로 직접 Bootstrap Album 페이지를 개발하시면 됩니다.
+import React from "react";
+import Navigation from "./component/Navigator/Navigation";
+import Top from "./component/top/Top";
+import Card from "./component/Content/Card";
+import Footer from "./component/Footer/Footer";
+
+import { GlobalStyle } from "./component/Styled/Styled";
+
 function App() {
-  return <>모두 화이팅!</>;
+  return <>  
+  <GlobalStyle/>
+  <Navigation></Navigation>
+  <Top/>
+  <Card/>
+  <Footer/>
+  </>;
 }
 
 export default App;
